f0bbc04f7dbb47aa368a70be726141a1c4ac9c3f
Maria <mevogy@hotmail.com>
2016-07-22 22:52:40 -0500

    Work on obtaining a GET request

--- app/src/main/java/com/maria/pokemon/MainActivity.java
+++ app/src/main/java/com/maria/pokemon/MainActivity.java
@@ -1,15 +1,29 @@
 package com.maria.pokemon;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
+
+    private MenuItem item;
+    private String url = "http://pokeapi.co/api/v2/evolution-chain/1/";
+    EditText rspText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,12 +40,27 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        rspText = (EditText) findViewById(R.id.showOutput);
+        Button getButton = (Button) findViewById(R.id.sendGet);
+        getButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                String name = rspText.getText().toString();
+                item.setActionView(R.layout.progress);
+                SendHttpRequestTask t = new SendHttpRequestTask();
+
+                String[] params = new String[]{url, name};
+                t.execute(params);
+            }
+        });
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
+        item = menu.getItem(0);
         return true;
     }
 
@@ -46,7 +75,51 @@ public class MainActivity extends AppCompatActivity {
         if (id == R.id.action_settings) {
             return true;
         }
-
         return super.onOptionsItemSelected(item);
+    }
+
+    private class SendHttpRequestTask extends AsyncTask<String, Void, String> {
+
+        @Override
+        protected String doInBackground(String... params) {
+            String url = params[0];
+            String name = params[1];
+
+            String data = sendHttpRequest(url, name);
+            System.out.println("Data ["+data+"]");
+            return data;
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            rspText.setText(result);
+            item.setActionView(null);
+
+        }
+    }
+
+    private String sendHttpRequest(String url, String name) {
+        StringBuffer buffer = new StringBuffer();
+        try {
+            System.out.println("URL ["+url+"] - Name ["+name+"]");
+
+            HttpURLConnection con = (HttpURLConnection) ( new URL(url)).openConnection();
+            con.setRequestMethod("GET");
+            con.connect();
+            con.getOutputStream().write( ("name=" + name).getBytes());
+
+            InputStream is = con.getInputStream();
+            byte[] b = new byte[1024];
+
+            while ( is.read(b) != -1)
+                buffer.append(new String(b));
+
+            con.disconnect();
+        }
+        catch(Throwable t) {
+            t.printStackTrace();
+        }
+
+        return buffer.toString();
     }
 }
